13aecd31c2d82c06cde0ad37e20ee32b274125ad
Maxim Mazurok <maxim@mazurok.com>
2022-06-27 18:07:05 +1000

    vitest

--- src/components/HelloWorld.spec.tsx
+++ src/components/HelloWorld.spec.tsx
@@ -1,5 +1,6 @@
 import { mount } from "@vue/test-utils";
 import HelloWorld from "./HelloWorld";
+import { describe, it, expect } from "vitest";
 
 describe("componentFactory", () => {
   describe("create", () => {
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { createVuePlugin as Vue2 } from "vite-plugin-vue2";
+
+export default defineConfig({
+  plugins: [Vue2()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./test/setup.ts"],
+    deps: {
+      inline: ["vuetify"],
+    },
+    clearMocks: true,
+  },
+});
