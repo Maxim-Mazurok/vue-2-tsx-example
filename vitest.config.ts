4737fc28448150c3eb651947621c379a650a80fc
Maxim Mazurok <maxim@mazurok.com>
2022-06-27 19:11:06 +1000

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import { createVuePlugin as Vue2 } from "vite-plugin-vue2";
 
 export default defineConfig({
-  plugins: [Vue2()],
+  plugins: [Vue2({ jsx: true })],
   test: {
     environment: "jsdom",
     setupFiles: ["./test/setup.ts"],
